edcd717c10caa6ca5788227c80ec8f0245b4408b
Emilio Scodeler <emilio.scodeler@solides.com.br>
2022-11-13 19:10:48 -0300

    clean up welcome tsdx story

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,1 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react';
-
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-  children?: ReactChild;
-}
-
-export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
-};
-
 export * from './components/RdButton';
